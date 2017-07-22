153e00f2adceb446d5f0ea6a7d35ad3d414239a6
onurcekeer <onur28695@gmail.com>
2017-07-22 19:32:50 +0300

    profile page designed

--- app/src/main/java/com/example/onur/loginexample/fragment/ThreeFragment.java
+++ app/src/main/java/com/example/onur/loginexample/fragment/ThreeFragment.java
@@ -1,8 +1,18 @@
 package com.example.onur.loginexample.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatCallback;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
@@ -10,7 +20,7 @@ import android.widget.Toast;
 import com.example.onur.loginexample.R;
 
 
-public class ThreeFragment extends Fragment {
+public class ThreeFragment extends Fragment{
 
 
     public ThreeFragment() {
@@ -28,7 +38,25 @@ public class ThreeFragment extends Fragment {
 
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_three, container, false);
+        View view = inflater.inflate(R.layout.fragment_three, container, false);
+        perform(view);
+        return view;
     }
+
+    public void perform(View v) {
+
+
+        FloatingActionButton fab = (FloatingActionButton) v.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ////
+            }
+        });
+
+
+    }
+
 
 }
