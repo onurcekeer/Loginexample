c61ac66ff5387842d4c54faabe049af08b88a362
onurcekeer <onur28695@gmail.com>
2017-07-23 02:08:02 +0300

    Firebase connection

--- app/src/main/java/com/project/onur/playerx/ConnectionChangeReceiver.java
+++ app/src/main/java/com/project/onur/playerx/ConnectionChangeReceiver.java
@@ -4,8 +4,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.support.design.widget.Snackbar;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -17,14 +15,9 @@ public class ConnectionChangeReceiver extends BroadcastReceiver
 {
     public void onReceive(Context context, Intent intent) {
         Log.d("app","Network connectivity change");
-        if(intent.getExtras()!=null) {
-            NetworkInfo ni=(NetworkInfo) intent.getExtras().get(ConnectivityManager.EXTRA_NETWORK_INFO);
-            if(ni!=null && ni.getState()==NetworkInfo.State.CONNECTED) {
-                Toast.makeText(context,"Network "+ni.getTypeName()+" connected",Toast.LENGTH_SHORT).show();
-            }
-        }
+
         if(intent.getExtras().getBoolean(ConnectivityManager.EXTRA_NO_CONNECTIVITY,Boolean.FALSE)) {
-            Toast.makeText(context,"internet bağlantısı yok",Toast.LENGTH_SHORT).show();
+            Toast.makeText(context,"internet bağlantısı yok",Toast.LENGTH_LONG).show();
         }
     }
 
