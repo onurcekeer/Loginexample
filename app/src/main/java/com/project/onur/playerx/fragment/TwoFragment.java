b184c08c37064a6116555834c3fbd88b6f98f861
onurcekeer <onur28695@gmail.com>
2017-08-14 18:18:14 +0300

    toolbar added in two fragment

--- app/src/main/java/com/project/onur/playerx/fragment/TwoFragment.java
+++ app/src/main/java/com/project/onur/playerx/fragment/TwoFragment.java
@@ -2,6 +2,8 @@ package com.project.onur.playerx.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,7 +30,16 @@ public class TwoFragment extends Fragment {
 
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_two, container, false);
+        View view =  inflater.inflate(R.layout.fragment_two, container, false);
+        perform(view);
+        return view;
+    }
+
+    private void perform(View v) {
+
+        Toolbar toolbar = (Toolbar) v.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        toolbar.setTitle(getString(R.string.title_messages));
     }
 
 }
