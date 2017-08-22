9ac668399bbef9a5451a0813c3414f78fb749d91
onurcekeer <onur28695@gmail.com>
2017-08-23 00:48:22 +0300

    context menu camera section added

--- app/src/main/java/com/project/onur/playerx/fragment/SettingsFragment.java
+++ app/src/main/java/com/project/onur/playerx/fragment/SettingsFragment.java
@@ -1,13 +1,18 @@
 package com.project.onur.playerx.fragment;
 
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -22,26 +27,39 @@ import android.widget.TextView;
 
 import com.appyvet.rangebar.RangeBar;
 import com.facebook.login.LoginManager;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserInfo;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.project.onur.playerx.R;
 import com.project.onur.playerx.SQLiteUser;
 import com.project.onur.playerx.User;
 import com.project.onur.playerx.activity.LoginActivity;
 import com.squareup.picasso.Picasso;
 
+import java.io.ByteArrayOutputStream;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SettingsFragment extends Fragment {
 
 
+    private static final int CAMERA_REQUEST = 1888;
+
     User user;
     SQLiteUser sqLiteUser;
     FirebaseUser firebaseUser;
+    DatabaseReference mDatabase;
     FirebaseAuth mAuth;
     String new_fullname;
     String new_Url;
+    Bitmap bitmap_new,bitmap_previous;
     int new_range;
 
 
@@ -55,7 +73,6 @@ public class SettingsFragment extends Fragment {
     Button button_logout;
 
 
-
     public SettingsFragment() {
         // Required empty public constructor
     }
@@ -66,8 +83,7 @@ public class SettingsFragment extends Fragment {
 
         sqLiteUser = new SQLiteUser(getActivity().getApplicationContext());
         mAuth = FirebaseAuth.getInstance();
-
-
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         Cursor cursor = sqLiteUser.query();
         user = sqLiteUser.getUserFromSQLite(cursor);
@@ -76,9 +92,7 @@ public class SettingsFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
@@ -91,13 +105,14 @@ public class SettingsFragment extends Fragment {
 
 
         switch (item.getItemId()) {
-            case R.id.navigation_home:
-                //pushFragment(new OneFragment());
+            case R.id.camera:
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
                 break;
-            case R.id.navigation_messages:
+            case R.id.select_photo:
                 //pushFragment(new TwoFragment());
                 break;
-            case R.id.navigation_profile:
+            case R.id.remove_photo:
                 //pushFragment(new ThreeFragment());
                 break;
         }
@@ -138,10 +153,22 @@ public class SettingsFragment extends Fragment {
 
         edit_fullname.setText(user.getUsername());
         rangeBar.setSeekPinByValue(user.getRange());
+        image_profile.setDrawingCacheEnabled(true);
+        image_profile.buildDrawingCache();
+        bitmap_previous = image_profile.getDrawingCache();
+
+        bitmap_new = bitmap_previous;
 
 
 
 
+
+        button_save_changes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveChanges();
+            }
+        });
         button_logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -175,6 +202,94 @@ public class SettingsFragment extends Fragment {
         Intent intent = new Intent(getActivity(),LoginActivity.class);
         startActivity(intent);
     }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            bitmap_new = (Bitmap) data.getExtras().get("data");
+            image_profile.setImageBitmap(bitmap_new);
+            if(isOnline()){
+                uploadUserPhoto(bitmap_new);
+            }
+
+        }
+    }
+
+
+    private void uploadUserPhoto(final Bitmap bitmap){
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                byte[] data = baos.toByteArray();
+
+                FirebaseStorage storage = FirebaseStorage.getInstance();
+                StorageReference storageRef = storage.getReference();
+
+                UploadTask uploadTask = storageRef.child(user.getUserID()+"jpg").putBytes(data);
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+
+                        // TODO: add snackbar
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                        Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                        new_Url = downloadUrl.toString();
+                        user.setProfilURL(new_Url);
+                    }
+                });
+
+            }
+        }).start();
+
+    }
+
+
+
+    public void saveChanges(){
+
+        new_fullname= edit_fullname.getText().toString();
+        new_range= Integer.parseInt(rangeBar.getRightPinValue());
+
+        if(isOnline()){
+
+            if(user.getUsername().equals(new_fullname) || (user.getRange()!=new_range) || user.getProfilURL().equals(new_Url)){
+                user.setRange(new_range);
+                user.setUsername(new_fullname);
+
+
+                mDatabase.child("User").child(user.getUserID()).setValue(user);
+                SQLiteUser sqliteUser = new SQLiteUser(getContext());
+                sqliteUser.addUserToSQLite(user);
+
+            }
+        }
+        else {
+
+            // TODO: 22.8.2017 add snackbar
+        }
+
+
+    }
+
+    public boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+
+
+
+
+
+
+
+
 
 
 
